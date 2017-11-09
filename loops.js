72ad0c5fc5fb2a4a4b10aa7cc969c8d1ebaa716f
Matthew Robinson <robinson.me@gmail.com>
2017-11-09 04:45:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,10 @@
-for (let i = 0; i < 25; i++) {
-  if (i === 0) {
-    console.log("I am 1 strange loop.");
-  } else {
-    console.log("I am ${i} strange loops.");
-  }
+function forLoop() {
+  for (let i = 0; i < 25; i++) {
+    if (i === 0) {
+      console.log("I am 1 strange loop.");
+    } else {
+      console.log("I am ${i} strange loops.");
+    }
 
+  }
 }
